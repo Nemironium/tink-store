bc97f6fe1f05c5dfe177e7f90258fe47cc07fac6
nemironium <nemiron@outlook.com>
2021-05-11 21:43:10 +0300

    Add kotlinx.ProtoBuf serializator and sample

--- sample/src/main/kotlin/io/nemiron/example/MainActivity.kt
+++ sample/src/main/kotlin/io/nemiron/example/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import io.nemiron.storetk.serializer.KotlinxJsonSerializer
+import io.nemiron.storetk.serializer.KotlinxProtoBufSerializer
 import kotlinx.serialization.Serializable
 
 class MainActivity : AppCompatActivity() {
@@ -21,12 +22,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        testSerializableClass()
-        testIntData()
-        testNonSerializableClass()
+        /*testJsonSerializableClass()
+        testJsonIntData()
+        testJsonNonSerializableClass()*/
+        testProtoBufSerializableClass()
+        testProtoBufIntData()
+        testProtoBufNonSerializableClass()
     }
 
-    private fun testSerializableClass() {
+    private fun testJsonSerializableClass() {
         val serializer = KotlinxJsonSerializer()
         val testData = TestData("ISCS EAZY!!!")
         val serializedData = serializer.serialize(testData)
@@ -37,7 +41,7 @@ class MainActivity : AppCompatActivity() {
         Log.i(TAG, "deserializedData = $deserializedData")
     }
 
-    private fun testNonSerializableClass() {
+    private fun testJsonNonSerializableClass() {
         val serializer = KotlinxJsonSerializer()
         try {
             val wrongData = WrongData("ISCS EAZY!!!")
@@ -47,8 +51,40 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun testIntData() {
+    private fun testJsonIntData() {
         val serializer = KotlinxJsonSerializer()
+        val testInt = 1337
+        val serializedInt = serializer.serialize(testInt)
+        val deserializedInt = serializer.deserialize(serializedInt, Int::class)
+
+        Log.i(TAG, "testInt = $testInt")
+        Log.i(TAG, "serializedInt = $serializedInt")
+        Log.i(TAG, "deserializedInt = $deserializedInt")
+    }
+
+    private fun testProtoBufSerializableClass() {
+        val serializer = KotlinxProtoBufSerializer()
+        val testData = TestData("ISCS EAZY!!!")
+        val serializedData = serializer.serialize(testData)
+        val deserializedData = serializer.deserialize(serializedData, TestData::class)
+
+        Log.i(TAG, "testData = $testData")
+        Log.i(TAG, "serializedData = $serializedData")
+        Log.i(TAG, "deserializedData = $deserializedData")
+    }
+
+    private fun testProtoBufNonSerializableClass() {
+        val serializer = KotlinxProtoBufSerializer()
+        try {
+            val wrongData = WrongData("ISCS EAZY!!!")
+            serializer.serialize(wrongData)
+        } catch (e: Exception) {
+            Log.e(TAG, "exception: $e")
+        }
+    }
+
+    private fun testProtoBufIntData() {
+        val serializer = KotlinxProtoBufSerializer()
         val testInt = 1337
         val serializedInt = serializer.serialize(testInt)
         val deserializedInt = serializer.deserialize(serializedInt, Int::class)
--- store-tink/src/main/kotlin/io/nemiron/storetk/serializer/KotlinxProtoBufSerializer.kt
+++ store-tink/src/main/kotlin/io/nemiron/storetk/serializer/KotlinxProtoBufSerializer.kt
@@ -2,27 +2,24 @@ package io.nemiron.storetk.serializer
 
 import io.nemiron.storetk.common.Data
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.json.Json
+import kotlinx.serialization.protobuf.ProtoBuf
 import kotlinx.serialization.serializer
 import kotlin.reflect.KClass
 
-class KotlinxJsonSerializer(
-    private val json: Json = Json
+class KotlinxProtoBufSerializer(
+    private val protoBuf: ProtoBuf = ProtoBuf
 ) : StoreTkSerializer {
 
     override fun <T : Any> serialize(value: T): Data {
         @Suppress("UNCHECKED_CAST")
         val classSerializer = value::class.serializer() as KSerializer<T>
-        val serializedBytes = json.encodeToString(classSerializer, value).encodeToByteArray()
+        val serializedBytes = protoBuf.encodeToByteArray(classSerializer, value)
         return Data(serializedBytes)
     }
 
     override fun <T : Any> deserialize(data: Data, kClass: KClass<T>): T {
         val classSerializer = kClass.serializer()
-        val serializedJsonString = data.value.decodeToString()
-        return json.decodeFromString(classSerializer, serializedJsonString)
+        val serializedByteArray = data.value
+        return protoBuf.decodeFromByteArray(classSerializer, serializedByteArray)
     }
 }
-
-
-
