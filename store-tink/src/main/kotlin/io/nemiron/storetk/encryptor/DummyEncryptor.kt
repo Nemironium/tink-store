a8e2a5c7e3546f1f6e565f9adac9764fca67960b
nemironium <nemiron@outlook.com>
2021-05-22 22:32:42 +0300

    Add InMemoryStorer for testing purposes

--- store-tink/src/main/kotlin/io/nemiron/storetk/encryptor/DummyEncryptor.kt
+++ store-tink/src/main/kotlin/io/nemiron/storetk/encryptor/DummyEncryptor.kt
@@ -1,6 +1,7 @@
 package io.nemiron.storetk.encryptor
 
 object DummyEncryptor : StoreTkEncryptor {
+
     override fun encrypt(value: ByteArray) = value
 
     override fun decrypt(encryptedValue: ByteArray) = encryptedValue
